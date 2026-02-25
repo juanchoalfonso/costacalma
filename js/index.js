e470243cd95e13166831692c95d9cf5b6ca75fe2
juanchoalfonso <juanmalfonso22@gmail.com>
2026-02-25 13:11:41 -0300

    favicon, top banner y fonts

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
+// Funcionalidad para el menú de navegación en celulares
+const mobileMenuButton = document.getElementById('mobile-menu');
+const navLinks = document.getElementById('nav-links');
 
+mobileMenuButton.addEventListener('click', () => {
+    // Agrega o quita la clase 'active' para mostrar/ocultar el menú
+    navLinks.classList.toggle('active');
+});
